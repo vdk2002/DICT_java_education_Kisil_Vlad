4a375fa803d3c66f4501eda340e4502fcfe3652e
vdk2002 <vlad2002kslgmail.com>
2023-01-18 21:04:33 +0300

    st-1 coffemachine

---
+++ src/coffemachine/CM.java
@@ -0,0 +1,8 @@
+import CoffeeMachine.CoffeeMachine;
+
+public class CM {
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.printText();
+    }
+}
---
+++ src/coffemachine/coffemachine.java
@@ -0,0 +1,13 @@
+package CoffeeMachine;
+
+public class CoffeeMachine {
+    public void printText() {
+        System.out.println("Starting to make a coffee\n" +
+                "Grinding coffee beans\n" +
+                "Boiling water\n" +
+                "Mixing boiled water with crushed coffee beans\n" +
+                "Pouring coffee into the cup\n" +
+                "Pouring some milk into the cup\n" +
+                "Coffee is ready!\n");
+    }
+}
