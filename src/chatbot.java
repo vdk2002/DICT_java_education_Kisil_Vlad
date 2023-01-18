c1c651ddad586005b845c5bb0672def556d43e29
vdk2002 <vlad2002kslgmail.com>
2023-01-18 20:52:12 +0300

    st2 chatbot

--- src/chatbot.java
+++ src/chatbot.java
@@ -1,8 +1,17 @@
 package ChatBot;
+import java.util.Scanner;
 
 public class chatbot {
     public void botName() {
         System.out.println("Hello! My name is Vitaliy");
         System.out.println("I was created in 2222");
+        System.out.println("Please, remind me your name.");
+        String name = scannerString();
+        System.out.println("What a great name you have, " + name + "!");
+    }
+    public String scannerString() {
+        try (Scanner scanner = new Scanner(System.in)){
+            return scanner.nextLine();
+        }
     }
 }
