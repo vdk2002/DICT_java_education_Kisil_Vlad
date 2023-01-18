80e737dc7a3cea695bf9bbd5c87fad74304a181c
vdk2002 <vlad2002kslgmail.com>
2023-01-18 20:59:16 +0300

    st3 hangman

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,7 +1,9 @@
 package Hangman;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Hangman {
     private String[] words = new String[]{"python", "java", "javascript", "kotlin"};
@@ -13,12 +15,22 @@ public class Hangman {
         System.out.println("HANGMAN");
     }
 
+    public String hint(String word) {
+        String[] result = word.split("");
+        for (int i = 2; i < result.length; i++) {
+            result[i] = "-";
+        }
+        return String.join("", result);
+    }
+
+
     public void guessWord() {
         Scanner in = new Scanner(System.in);
-        System.out.print("Guess the word:> ");
+        String randomWords = words[randomIndex];
+        System.out.print("Guess the word " + hint(randomWords) + ":> ");
         String userWord = in.next();
         in.close();
-        if (userWord.equals(words[randomIndex])) {
+        if (userWord.equals(randomWords)) {
             System.out.println("You survived!");
         } else {
             System.out.println("You lost!");
