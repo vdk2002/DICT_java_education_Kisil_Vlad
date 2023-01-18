bde727f337752caefbb4a8d34fd395d437694f70
vdk2002 <vlad2002kslgmail.com>
2023-01-18 20:51:34 +0300

    st1 chatbot

---
+++ src/chatbot.java
@@ -0,0 +1,8 @@
+package ChatBot;
+
+public class chatbot {
+    public void botName() {
+        System.out.println("Hello! My name is Vitaliy");
+        System.out.println("I was created in 2222");
+    }
+}
